ad9585809e6cfe722d11c32661a762e2a6ccba50
Piyush <piyushjain1sa@gmail.com>
2017-08-02 22:29:57 +0530

    synced with matGeom

--- inst/dummyDir1/dummyScript2.m
+++ inst/dummyDir1/dummyScript2.m
@@ -1,10 +1,10 @@
-# Copyright - Somebody
-# Bla bla
-# bla bla bla
-
-# texinfo
-# Help of function
+% Bla bla
+% bla bla bla
 
 function z = blabla (x,y)
-   z = x .* y;
-endfunction
+% Help of function
+% updated to be more clear
+z = zeros (size(x));
+for i=1:length(x)
+   z(i) = x(i)*y(i);
+end
